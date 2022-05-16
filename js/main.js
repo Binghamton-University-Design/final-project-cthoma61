391d6eb2512806b76eeede4bac8cdb2878b5e7ad
cthoma61 <98545222+cthoma61@users.noreply.github.com>
2022-05-16 10:34:47 -0400

    1

--- js/main.js
+++ js/main.js
@@ -6,17 +6,3 @@ console.log('hello');
 
 });
 
-<script>
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar-items");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-</script>
