d57c0212797cdc9052d9ae46a15101db315220d0
cthoma61 <98545222+cthoma61@users.noreply.github.com>
2024-01-25 11:40:00 -0500

    2024 1/25 Big Changes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function() {
 
-console.log('hello');
-
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
@@ -23,12 +21,9 @@ menu.addEventListener('click', mobileMenu);
   const aboutMenu = document.querySelector('#about-page');
   const servicesMenu = document.querySelector('#services-page');
   let scrollPos = window.scrollY;
-  // console.log(scrollPos);
 
   // adds 'highlight' class to my menu items
   if (window.innerWidth > 960 && scrollPos < 600) {
-    homeMenu.classList.add('highlight');
-    aboutMenu.classList.remove('highlight');
     return;
   } else if (window.innerWidth > 960 && scrollPos < 1400) {
     aboutMenu.classList.add('highlight');
@@ -61,16 +56,3 @@ const hideMobileMenu = () => {
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu);
 });
-
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("container");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
