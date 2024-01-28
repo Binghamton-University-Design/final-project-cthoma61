531aa980bc759ffd3853cd7401a11ed5c8699f54
cthoma61 <98545222+cthoma61@users.noreply.github.com>
2024-01-28 15:34:20 -0500

    nav mobile addition

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ $(document).ready(function() {
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
+const outside = document.querySelector('#outsidenav')
 
 
 // Display Mobile Menu
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+  outside.classList.toggle('active')
 };
 
 menu.addEventListener('click', mobileMenu);
@@ -23,9 +25,11 @@ const hideMobileMenu = () => {
   if (window.innerWidth <= 768 && menuBars) {
     menu.classList.toggle('is-active');
     menuLinks.classList.remove('active');
+    outside.classList.remove('active')
   }
 };
 
-menuLinks.addEventListener('click', hideMobileMenu);
+
 navLogo.addEventListener('click', hideMobileMenu);
+outside.addEventListener('click', hideMobileMenu)
 });
