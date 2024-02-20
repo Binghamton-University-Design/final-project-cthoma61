a4563e113d53ea5ea816efcee89d98090fc0edbb
cthoma61 <98545222+cthoma61@users.noreply.github.com>
2024-02-20 16:31:12 -0500

    1

--- js/main.js
+++ js/main.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
-const outside = document.querySelector('.outsidenav')
+const outside = document.querySelector('.outsidenav');
 
 
 // Display Mobile Menu
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
-  outside.classList.toggle('active')
+  outside.classList.toggle('active');
 };
 
 menu.addEventListener('click', mobileMenu);
@@ -25,11 +25,34 @@ const hideMobileMenu = () => {
   if (window.innerWidth <= 820 && menuBars) {
     menu.classList.toggle('is-active');
     menuLinks.classList.remove('active');
-    outside.classList.remove('active')
+    outside.classList.remove('active');
   }
 };
 
 
 navLogo.addEventListener('click', hideMobileMenu);
-outside.addEventListener('click', hideMobileMenu)
+outside.addEventListener('click', hideMobileMenu);
+
+
+
+
+// Show button when scrolling down
+const arrowUp = $('#arrowup');
+
+$(window).on('scroll', () => {
+  if ($(window).scrollTop() > 500) {
+    arrowUp.show();
+  } else {
+    arrowUp.hide();
+  }
+});
+
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+        $(window).scrollTop(0);
+        }
+
+
+arrowUp.on('click', scrollToTop)
 });
